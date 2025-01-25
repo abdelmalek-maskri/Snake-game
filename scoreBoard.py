55ce92ba2552dfb65a39dbc1c906e2bd768b2ef3
Abdelmalek Maskri <abdelmalekmaskri18@gmail.com>
2025-01-25 23:09:30 +0000

    added high score plus re-play automatically

--- scoreBoard.py
+++ scoreBoard.py
@@ -7,18 +7,30 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highScore = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center", font=FONT)
+        self.updateScore()
         self.hideturtle()
+
+
+    def updateScore(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highScore}", align="center", font=FONT)
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=FONT)
+        self.updateScore()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=FONT)
+    def reset(self):
+        if self.score > self.highScore:
+            self.highScore = self.score
+        self.score = 0
+        self.updateScore()
+
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write("GAME OVER", align="center", font=FONT)
         
